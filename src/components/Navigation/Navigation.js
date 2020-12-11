96b58519e6626a1022c75d7be959e5dc9cdddb61
julieg18 <juliannagal18@gmail.com>
2020-12-11 06:36:31 -0600

    fix: add missing overlay to Navigation dropdown

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,33 +6,35 @@ function Navigation() {
     <nav className="navigation">
       <p className="navigation__title">News Explorer</p>
       <button className="navigation__mobile-btn navigation__mobile-btn_icon_menu"></button>
-      <div className="navigation__dropdown">
-        <div className="navigation__dropdown-nav-bar">
-          <p className="navigation__title navigation__dropdown-title">
-            News Explorer
-          </p>
-          <button className="navigation__mobile-btn navigation__mobile-btn_icon_exit"></button>
+      <div className="overlay">
+        <div className="navigation__dropdown">
+          <div className="navigation__dropdown-nav-bar">
+            <p className="navigation__title navigation__dropdown-title">
+              News Explorer
+            </p>
+            <button className="navigation__mobile-btn navigation__mobile-btn_icon_exit"></button>
+          </div>
+          <ul className="navigation__dropdown-items">
+            <li className="navigation__item">
+              <a href="#home" className="navigation__link">
+                Home
+              </a>
+            </li>
+            <li className="navigation__item navigation__item_hidden">
+              <a href="#articles" className="navigation__link">
+                Saved Articles
+              </a>
+            </li>
+            <li className="navigation__item">
+              <button className="navigation__btn">Sign In</button>
+            </li>
+            <li className="navigation__item navigation__item_hidden">
+              <button className="navigation__btn">
+                Name <span className="navigation__btn-icon"></span>
+              </button>
+            </li>
+          </ul>
         </div>
-        <ul className="navigation__dropdown-items">
-          <li className="navigation__item">
-            <a href="#home" className="navigation__link">
-              Home
-            </a>
-          </li>
-          <li className="navigation__item navigation__item_hidden">
-            <a href="#articles" className="navigation__link">
-              Saved Articles
-            </a>
-          </li>
-          <li className="navigation__item">
-            <button className="navigation__btn">Sign In</button>
-          </li>
-          <li className="navigation__item navigation__item_hidden">
-            <button className="navigation__btn">
-              Name <span className="navigation__btn-icon"></span>
-            </button>
-          </li>
-        </ul>
       </div>
       <ul className="navigation__items">
         <li className="navigation__item">
