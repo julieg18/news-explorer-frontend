e802a5f4bc84221d70c7fa4cf8d80d1d309cd81f
julieg18 <juliannagal18@gmail.com>
2020-12-19 18:27:06 -0600

    fix: add missing features in NewsCard * keep btn from activating link * add card styling for when news card is in saved articles section * add card btn tooltips

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import './NewsCard.css';
 
-function NewsCard({ card: { title, text, date, source, link, image } }) {
+function NewsCard({
+  isCardInMain,
+  isUserLoggedIn,
+  card: { title, text, date, source, link, image, keyword, saved },
+}) {
+  function handleBtnClick(e) {
+    e.preventDefault();
+  }
+
   return (
     <a href={link} target="_blank" rel="noreferrer" className="news-card">
       <img className="news-card__img" src={image} alt={title} />
-      <button className="news-card__save-btn"></button>
+      {isCardInMain ? (
+        <>
+          <button
+            onClick={handleBtnClick}
+            className={`news-card__btn news-card__btn_icon_${
+              saved ? 'blue-' : ''
+            }bookmark`}
+          ></button>
+          <span
+            className={`news-card__btn-tooltip ${
+              isUserLoggedIn && 'news-card__btn-tooltip_hidden'
+            }`}
+          >
+            Sign in to save articles
+          </span>
+        </>
+      ) : (
+        <>
+          <span className="news-card__keyword">{keyword}</span>
+          <button
+            onClick={handleBtnClick}
+            className="news-card__btn news-card__btn_icon_trash"
+          ></button>
+          <span className="news-card__btn-tooltip">Remove from saved</span>
+        </>
+      )}
       <p className="news-card__date">{date}</p>
       <h3 className="news-card__title">{title}</h3>
       <p className="news-card__text">{text}</p>
