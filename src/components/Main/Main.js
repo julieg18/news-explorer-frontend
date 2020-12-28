0b29500ce7fcb932ee6c4d4afe7901c8dcd91aac
julieg18 <juliannagal18@gmail.com>
2020-12-27 18:33:37 -0600

    fix: edit Main sections according to site spec

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,31 +7,45 @@ import './Main.css';
 
 function Main({ cards, isUserLoggedIn }) {
   const [isLoading] = useState(false);
+  const [displayedCards] = useState(cards.slice(0, 3));
 
   return (
     <main>
       <section className="search-results">
-        <h2 className="search-results__title">Search Results</h2>
-        {!isLoading && cards.length === 0 ? (
-          <p className="search-results__no-results-text">No results found</p>
+        {isLoading ? (
+          <>
+            <div className="search-results__preloader">
+              <Preloader />
+            </div>
+            <p className="search-results__preloader-text">
+              Searching for news...
+            </p>
+          </>
+        ) : cards.length === 0 ? (
+          <>
+            <div className="search-results__not-found-icon"></div>
+            <h2 className="search-results__not-found-title">Nothing found</h2>
+            <p className="search-results__not-found-text">
+              Sorry, but nothing matched your search terms.
+            </p>
+          </>
         ) : (
           <>
+            <h2 className="search-results__title">Search Results</h2>
             <NewsCardList
               areCardsInMain
-              cards={cards}
+              cards={displayedCards}
               isUserLoggedIn={isUserLoggedIn}
             />
-            {isLoading ? (
-              <div className="search-results__preloader">
-                <Preloader />
-              </div>
-            ) : (
+            {displayedCards.length !== cards.length ? (
               <Button
                 lightTheme
                 additionalClasses="search-results__show-more-btn"
               >
                 Show More
               </Button>
+            ) : (
+              ''
             )}
           </>
         )}
