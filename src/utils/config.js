bd227411c6696edcf4de0838b7c8fe20b4b93044
julieg18 <juliannagal18@gmail.com>
2021-01-02 13:56:53 -0600

    fix: update mainApiBaseUrl

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ const displayedCardsAmount = 3;
 const newsApiAuthorizationKey = '7a87f53207154e3698b4738295e2f3a0';
 const newsApiBaseUrl =
   'https://nomoreparties.co/news/v2/everything?language=en';
-const mainApiBaseUrl = 'https://news-explorer-297322.uc.r.appspot.com/api';
+const mainApiBaseUrl = '/api';
 
 export {
   newsSearchDays,
